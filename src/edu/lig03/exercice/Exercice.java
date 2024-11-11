77b8a5888acc343f1af5850c773821d83f515142
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2024-11-11 15:20:23 +0100

    add

--- src/edu/lig03/exercice/Exercice.java
+++ src/edu/lig03/exercice/Exercice.java
@@ -1,7 +1,15 @@
 package edu.lig03.exercice;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 import edu.lig03.utilitaire.Lecteur;
 
@@ -173,58 +181,159 @@ public class Exercice {
 		int n = Lecteur.lireEntier("Quelle table de multiplication souhaitez-vous réviser ?");
 		// Demande et assignation du nombre de question voulu
 		int nbQuestion = Lecteur.lireEntier("Combien de questions souhaitez-vous ?");
-		//boucle for car nous savons combien d'itération il va y avoir
+		// boucle for car nous savons combien d'itération il va y avoir
 		for (int i = 1; i <= nbQuestion; i++) {
 			// Demande et assignation des réponses
 			int response = Lecteur.lireEntier(i + "/" + nbQuestion + "Combien font " + i + "x" + n + "?");
 			// verifie si la réponse est correcte
 			if (response == i * n) {
-				//Augmente le compteur de bonne réponse
+				// Augmente le compteur de bonne réponse
 				goodResponse++;
 			}
-			//Annonce le nombre de bonne réponse sur le nombre total de quetion posées
+			// Annonce le nombre de bonne réponse sur le nombre total de quetion posées
 			System.out.println("Vous obtenez la note de " + goodResponse + " sur " + nbQuestion);
 
 		}
 	}
 
 	public static void exercice8() {
-		 // Demande de saisie de la chaîne de caractères
+		// Demande de saisie de la chaîne de caractères
 		String phrase = Lecteur.lireTexte("Entrez une phrase à tester pour le palindrome: ");
 
-        String phraseSansAccent = supprimerAccents(phrase);
+		String phraseSansAccent = supprimerAccents(phrase);
 
-        // Appel de la fonction palindrome et affichage du résultat
-        boolean estPalindrome = palindrome(phraseSansAccent);
-        if (estPalindrome) {
-            System.out.println("La phrase est un palindrome.");
-        } else {
-            System.out.println("La phrase n'est pas un palindrome.");
-        }
-        
-    }
+		// Appel de la fonction palindrome et affichage du résultat
+		boolean estPalindrome = palindrome(phraseSansAccent);
+		if (estPalindrome) {
+			System.out.println("La phrase est un palindrome.");
+		} else {
+			System.out.println("La phrase n'est pas un palindrome.");
+		}
+
+	}
 
 	// Fonction qui détermine si une chaîne est un palindrome.
 
-    public static boolean palindrome(String chaineATester) {
-        // Normalisation de la chaîne pour ignorer les espaces, la ponctuation, et la casse
-        String normalisee = chaineATester.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+	public static boolean palindrome(String chaineATester) {
+		// Normalisation de la chaîne pour ignorer les espaces, la ponctuation, et la
+		// casse
+		String normalisee = chaineATester.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
 
-        // Vérification du palindrome en comparant la chaîne avec son inverse
-        String inverse = new StringBuilder(normalisee).reverse().toString();
-        return normalisee.equals(inverse);
-    }
-    
-    //Fonction qui supprime les accents d'une chaîne.
-    public static String supprimerAccents(String chaine) {
-        // Utilisation de Normalizer pour séparé les accent de la phrase
-        String sansAccents = Normalizer.normalize(chaine, Normalizer.Form.NFD);
-        // Permet d'enlever tout les caractére qui ne sont pas des lettres
-        sansAccents = sansAccents.replaceAll("[^\\p{ASCII}]", "");
-        // Retourne la phrase sans accent
-        return sansAccents;
-    }
-	
+		// Vérification du palindrome en comparant la chaîne avec son inverse
+		String inverse = new StringBuilder(normalisee).reverse().toString();
+		return normalisee.equals(inverse);
+	}
+
+	// Fonction qui supprime les accents d'une chaîne.
+	public static String supprimerAccents(String chaine) {
+		// Utilisation de Normalizer pour séparé les accent de la phrase
+		String sansAccents = Normalizer.normalize(chaine, Normalizer.Form.NFD);
+		// Permet d'enlever tout les caractére qui ne sont pas des lettres
+		sansAccents = sansAccents.replaceAll("[^\\p{ASCII}]", "");
+		// Retourne la phrase sans accent
+		return sansAccents;
+	}
+
+	public static void exercice9() {
+		/*
+		 * Chemin du fichier Doit bien faire refernce à l'emplacement du fichier
+		 */
+		String cheminFichier = "C:\\Users\\torrs\\Documents\\GitHub\\exercice\\src\\edu\\lig03\\exercice\\liste_mot_francais.txt";
+		// Création d'une liste pour stocker les mots présent dans le fichier
+		List<String> mots = new ArrayList<>();
+		// Test pour verifier la presence du fichier au chemin specifier
+		try (BufferedReader lecteur = new BufferedReader(new FileReader(cheminFichier))) {
+			String ligne;
+			// Tant qu'une ligne existe la lit
+			while ((ligne = lecteur.readLine()) != null) {
+				// Met la ligne en miniscule
+				ligne.toLowerCase();
+				// Ajout de la ligne dans notre liste
+				mots.add(ligne);
+			}
+			// Affiche une erreur si le fichier n'est pas présent
+		} catch (IOException e) {
+			System.err.println("Erreur de lecture du fichier : " + e.getMessage());
+		}
+		Random rand = new Random();
+		// Prend un mot aleatoire dans notre liste et prend la teille de la liste pour
+		// permettre de futur changement
+		String mot = mots.get(rand.nextInt(mots.size()));
+		List<Character> propositions = new ArrayList<>();
+		//System.out.println(mot);
+		etatDuMot(mot, propositions);
+		int erreurs = 0;
+		//fait tourner la boucle indefiniment jusqu'a atteindre un break
+		while (true) {
+			//Utilise la fonction afficherPendu
+			afficherPendu(erreurs);
+			//Condition de défaite
+			if (erreurs >= 6) {
+				System.out.println("Vous avez perdu!");
+				break;
+			}
+			//Utilise la fonction enregsitreProposition
+			if (!enregsitreProposition(mot, propositions)) {
+				erreurs++;
+			}
+			//Utilise la fonction etatDuMot
+			if (etatDuMot(mot, propositions)) {
+				System.out.println("Vous avez gagné!");
+				break;
+			}
+
+		}
+
+	}
+	//Fonction pour afficher l'évolution du pendu
+	private static void afficherPendu(int erreurs) {
+		System.out.println(" _______");
+		System.out.println(" |     |");
+		if (erreurs >= 1) {
+			System.out.println(" O");
+		}
+		if (erreurs >= 2) {
+			System.out.print("\\ ");
+			if (erreurs >= 3) {
+				System.out.println("/");
+			} else {
+				System.out.println();
+			}
+		}
+		if (erreurs >= 4) {
+			System.out.println(" |");
+		}
+		if (erreurs >= 5) {
+			System.out.print("/ ");
+			if (erreurs >= 6) {
+				System.out.println("\\");
+			} else {
+				System.out.println();
+			}
+		}
+	}
+	//Fonction qui enregistre les propositions de l'utilisateur
+	private static boolean enregsitreProposition(String mot, List<Character> propositions) {
+		String lettrePropose = Lecteur.lireTexte("Choisir une lettre");
+		propositions.add(lettrePropose.charAt(0));
+
+		return mot.contains(lettrePropose);
+	}
+	//Fonction qui montre l'avancement de la découverte du mot
+	private static boolean etatDuMot(String mot, List<Character> propositions) {
+		int nombreLettreTrouve = 0;
+		for (int i = 0; i < mot.length(); i++) {
+			if (propositions.contains(mot.charAt(i))) {
+				System.out.print(mot.charAt(i));
+				nombreLettreTrouve++;
+			} else {
+				System.out.print("-");
+			}
+		}
+		System.out.println();
+		return (mot.length() == nombreLettreTrouve);
+	}
+
 	public static void main(String[] args) {
 //		 exercice1();
 //		 exercice2();
@@ -233,7 +342,8 @@ public class Exercice {
 //		exercice5();
 //		exercice6();
 //		exercice7();
-		exercice8();
+//		exercice8();
+		exercice9();
 	}
 
 }
